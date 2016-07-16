32476d37ca0bbf1b6bfdc9bfe128c717d493f376
xhu <hxtheone@gmail.com>
2016-07-16 14:16:26 +0800

    update gitignore and add stack implemented by scala

---
+++ 1/1.3/1.3.2.scala
@@ -0,0 +1,33 @@
+object Exercise2 {
+  class Stack[T](var data: List[T]) {
+    def push(arg: T): Stack[T] = {
+      this.data = arg +: this.data
+      return this
+    }
+
+    def pop(): String = {
+      val top = this.data match {
+        case head +: tail => {
+          this.data = tail
+          s"$head"
+        }
+        case Nil => "Nil"
+      }
+      return top
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    var a = new Stack[String](List())
+    a.push("it").push("was").push("the").push("best").push("of").push("times")
+    println(a.pop)
+    println(a.pop)
+    println(a.pop)
+    println(a.pop)
+    println(a.pop)
+    println(a.pop)
+    println(a.pop)
+    println(a.pop)
+    println(a.pop)
+  }
+}
