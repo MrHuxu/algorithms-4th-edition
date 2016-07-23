cc4728757aa43cb90d0abad8942335b5f7277bd6
xhu <hxtheone@gmail.com>
2016-07-23 23:52:56 +0800

    finish mergeSortBU

--- section2/unit2/mergeSortBU.go
+++ section2/unit2/mergeSortBU.go
@@ -11,7 +11,7 @@ func merge(nums []int, lo int, mid int, hi int) {
 	j := mid + 1
 
 	for k := lo; k <= hi; k++ {
-		aux = append(aux, nums[k])
+		aux[k] = nums[k]
 	}
 
 	for k := lo; k <= hi; k++ {
@@ -31,6 +31,32 @@ func merge(nums []int, lo int, mid int, hi int) {
 	}
 }
 
-func main() {
+func mergeSortBU(nums []int) {
+	for _, v := range nums {
+		aux = append(aux, v)
+	}
 
+	sortDetail(nums, 0, len(nums)-1)
+}
+
+func sortDetail(nums []int, lo int, hi int) {
+	N := len(nums)
+	for sz := 1; sz < N; sz *= 2 {
+		for lo := 0; lo < N-sz; lo += sz * 2 {
+			var tmp int
+			if lo+sz*2 < N {
+				tmp = lo + sz*2 - 1
+			} else {
+				tmp = N - 1
+			}
+			merge(nums, lo, lo+sz-1, tmp)
+		}
+	}
+}
+
+func main() {
+	var nums []int
+	nums = append(nums, 4, 3, 1, 3, 2, 5)
+	mergeSortBU(nums)
+	fmt.Println(nums)
 }
